87dea11b0cd73fe8fcc13e6e007c9ea283484093
apdo <apdo@abdo>
2024-12-18 20:46:11 +0200

    node notes

---
+++ node.js
@@ -0,0 +1,35 @@
+//global
+//process
+//global.process.argv
+//enternal external third party module
+//module.exports = { functionName,varname }
+// npm.com/package/image
+// let home = "house"
+// function addst (){}
+// package for error handling 
+// export  {home,addst}
+// import fs from 'fs' 
+// fs.promises.mkdir('')
+// fs.mkdir("./newFolder",{recursive:true},(error)=>{
+    // console.log(error)
+    // console.log('first')
+// })
+// console.log("secound")
+// fs.mkdirSync("./newFolder2/file/",{recursive:true},) //if newfolder2 is not found will be error
+
+// fs.rm('./newFolder',(err)=>{console.log(err)})
+
+// fs.rename()
+// fs.writeFile('./test.txt','hello',()=>{})
+//     fs.readFile('./test.txt','utf-8',(err,data)=>{
+//         console.log(data)
+//     })
+
+    // manage array of products (object) func add produ and delet with id edit with id  func for all prod
+    //func return product with id
+    //array will saved 
+    //first read file.json 
+    //edit arr 
+    //over ride the file;
+    // fs.unlink()
+    // fs.access('./test.txt',fs.constants.F_OK,)
